c7489aed822459816689c5ac6491b69d8e590b22
Samat Karymshakov <bskordo@gmail.com>
2017-04-22 14:36:20 +0600

    On README.md has been added description,requirements added

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Cinemas
+____
+> **The script will get movie name and in how many cinemas has been showing from afisha.ru and for each find rating and votes from kinopoisk.ru(on console will showed 10 movies which has largest rating). Warning!!! Most probably in second attempt your IP will be banned by kinopoisk.ru**
 
-[TODO. There will be project description]
+## Requirement
+Need to install usefull modules **beautifulsoup** and **requests**, for do it run command:
+```
+pip install -r requirements.txt
+```
+### Usage:
+On console run the script :
+```
+python3.5 cinemas.py
 
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
+____
+
+
--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,76 @@
+import requests
+import re
+import itertools
+from bs4 import BeautifulSoup
 
 
-def fetch_afisha_page():
-    pass
+def parse_afisha_page():
+    inf_from_url = requests.get('https://www.afisha.ru/msk/schedule_cinema/')
+    soup = BeautifulSoup(inf_from_url.content, 'lxml')
+    raw_afisha_inf = soup.find_all('div',{'class':'object s-votes-hover-area collapsed'})
+    return raw_afisha_inf
 
 
-def parse_afisha_list(raw_html):
-    pass
+def get_movie_name_cinema_count():
+    movie_name_cinema_count = []
+    movies = parse_afisha_page()
+    for movie in movies:
+        movie_store = {
+            'movie_name': movie.find('h3',{'class':'usetags'}).findNext('a').text,
+            'count_of_cinema': len(movie.find_all('tr'))}
+        movie_name_cinema_count.append(movie_store )
+    return movie_name_cinema_count
 
 
-def fetch_movie_info(movie_title):
-    pass
+def get_rating_and_counting_ball(movie):
+    payload = {'kp_query':movie, 'first':'yes'}
+    movie_html = requests.get('https://www.kinopoisk.ru/index.php', params=payload)
+    soup = BeautifulSoup(movie_html.content, 'lxml')
+    movie_kpi = {
+        'movie_rating': get_movie_rating(soup),
+        'movie_votes': get_movie_votes(soup)
+    }
+    return movie_kpi
 
 
-def output_movies_to_console(movies):
-    pass
+def get_movie_rating(movie):
+    rating = movie.find('span',{'class':'rating_ball'})
+    if rating:
+        return float(rating.text)
+    else:
+        return None
+
+
+def get_movie_votes(movie):
+    votes = movie.find('span',{'class':'ratingCount'})
+    if votes:
+        return int(re.sub('\s+','',votes.text))
+    else:
+        return None
+
+
+def collect_movie_information():
+    movies_store = []
+    movies = get_movie_name_cinema_count()
+    for movie in movies:
+        movie_rate = get_rating_and_counting_ball(movie['movie_name'])
+        movies_store.append({
+            'title': movie['movie_name'],
+            'cinema_count': movie['count_of_cinema'],
+            'rating': movie_rate['movie_rating'],
+            'votes': movie_rate['movie_votes']
+            })
+    return movies_store
+
+
+def show_to_console(movie_information):
+    _best_kpi = 10
+    sorted_movies = sorted(movie_information, key=lambda elem:(elem['rating'] is not None, elem['rating']),reverse=True)[:_best_kpi]
+    for movie in sorted_movies:
+        for attribute,value in movie.items():
+            print('{} : {}'.format(attribute, value))
 
 
 if __name__ == '__main__':
-    pass
+    movie_information = collect_movie_information()
+    show_to_console(movie_information)
